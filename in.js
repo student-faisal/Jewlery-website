55b6b8150e6b618a94b3aebbed1a07e4accd6d66
Your Name <you@example.com>
2023-12-07 03:15:56 -0800

    fourth message

---
+++ in.js
@@ -0,0 +1,12 @@
+function file(){
+
+    alert("Welcome to the Jewlery Website in this website give many expensive jewlery so pleaase see website and    " + "Order now "
+    +  "  As the most expensive piece of jewellery in the world, with an estimated value of $250 million, it's no surprise that the Hope Diamond has garnered many stories and myths over the years.");
+}
+function icone(){
+      var a=["Shopping ","Order ","See Website ","Branded Compnay ","Golden Jewelery", 
+      "Silver Jewelery","Neclates","Golden Branded "];
+      alert( "this website provide the  following advantages" + " " + a );
+      
+}
+
